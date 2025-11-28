c3f8cfd9f6a53d1e9f3e961e706b60dd73460612
Claw the Dragon <clawschwic@gmail.com>
2025-11-28 08:06:16 -0600

    Added more base variables.

--- Source/Entity.rb
+++ Source/Entity.rb
@@ -1,16 +1,21 @@
 
 class Entity
   attr_accessor :HP, :Mana
+  attr_accessor :LV, :EXP, :MaxEXP
   attr_accessor :Strength, :Defence, :SpecialStrength, :SpecialDefence
 
-  def initialize(hp, mana, strength, defence, specialStrength, specialDefence)
+  def initialize(hp, mana, lv, exp, max_exp, strength, defence, special_strength, special_defence)
     self.HP   = hp
     self.Mana = mana
 
+    self.LV       = lv
+    self.EXP      = exp
+    self.MaxEXP   = max_exp
+
     self.Strength         = strength
     self.Defence          = defence
-    self.SpecialStrength  = specialStrength
-    self.SpecialDefence   = specialDefence
+    self.SpecialStrength  = special_strength
+    self.SpecialDefence   = special_defence
   end
 
 end
